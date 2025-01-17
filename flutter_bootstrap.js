79fba04d74cd3f066d766116a2106115bc9c0f30
Leonidas19 <leon.katsaros@gmail.com>
2025-01-17 18:43:46 +0000

    automated commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4101883273"
+    serviceWorkerVersion: "1899635676"
   }
 });
